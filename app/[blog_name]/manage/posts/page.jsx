81dc8aea2b3b21a1dfd11ddf8b75cd7c4227b6e3
VinylStage <mcpe9869@gmail.com>
2023-06-20 23:24:59 +0900

    :Docs edit  JSDocs
    2023.06.20
    params참조 페이지 주석수정

--- app/[blog_name]/manage/posts/page.jsx
+++ app/[blog_name]/manage/posts/page.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Posts from "@/components/Posts";
 
-/** 게시글 작성페이지 */
+/**
+ * 게시글 작성페이지
+ * @params {blog_name} 블로그 이름
+ */
 export default function posts({ params }) {
   return (
     <>
