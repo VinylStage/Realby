b89cc0e57464c3e9b4ff0ad9cbe4a84345dd7626
VinylStage <mcpe9869@gmail.com>
2023-06-23 14:47:57 +0900

    :(HOTFIX) Deploy Fix #25

--- app/api/auth/login/page.jsx
+++ app/api/auth/login/page.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LoginView from "../../../components/LoginView";
+import LoginView from "@/components/LoginView";
 
 /** 로그인페이지 */
 export default function login() {
