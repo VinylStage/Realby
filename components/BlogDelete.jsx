231cb0379bdbdcc85fa6ca24edf75619cca1703f
VinylStage <mcpe9869@gmail.com>
2023-06-26 11:05:21 +0900

    :(Fix) Article Detail, Subs Components #4, #5
    2023.06.26
    상세게시글 화면구현 수정
    구독/블로그삭제 컴포넌트 수정
    글쓰기 컴포넌트 일부수정(마크다운 태그변경)
    댓글 팝업창 연결URL수정

---
+++ components/BlogDelete.jsx
@@ -0,0 +1,41 @@
+"use client";
+
+import axios from "axios";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SendIcon from "@mui/icons-material/Send";
+import Stack from "@mui/material/Stack";
+
+/** 블로그 삭제버튼 */
+export default function BlogDelete({ blog_name: blog_name }) {
+  const handleBlogDelete = async () => {
+    const token = localStorage.getItem("access");
+    const response = await axios.delete(
+      `http://54.180.120.169/blogs/${blog_name}/`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+  };
+  return (
+    <>
+      <Stack direction="row" spacing={2}>
+        <section>
+          <form action="/">
+            <Button
+              variant="outlined"
+              startIcon={<DeleteIcon />}
+              type="submit"
+              onClick={handleBlogDelete}
+            >
+              Delete
+            </Button>
+          </form>
+        </section>
+      </Stack>
+    </>
+  );
+}
